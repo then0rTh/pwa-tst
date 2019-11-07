29e34fbbba6f9b4078218090cffdf9361a99a54a
then0rTh <nth.orx@gmail.com>
2019-11-07 14:16:06 +0100

    i

---
+++ worker.js
@@ -0,0 +1,39 @@
+
+
+const cacheName = 'pwa-tst-v1';
+const cacheFiles = [
+  '/pwa-tst/',
+  '/pwa-tst/index.html',
+  '/pwa-tst/main.js',
+  '/pwa-tst/main.css',
+  '/pwa-tst/icon.png',
+];
+
+
+self.addEventListener('install', (event) => {
+	console.log('[Service Worker] Install');
+	event.waitUntil(
+		caches.open(cacheName).then((cache) => {
+			console.log('[Service Worker] Caching all: app shell and content');
+			return cache.addAll(cacheFiles);
+		})
+	);
+});
+
+
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+		caches.match(event.request).then((request) => {
+			console.log(`[Service Worker] Fetching resource: ${event.request.url}`);
+			if (reuqest)
+				return request;
+			return fetch(event.request).then((response) => {
+				return caches.open(cacheName).then((cache) => {
+					console.log(`[Service Worker] Caching new resource: ${event.request.url}`);
+					cache.put(event.request, response.clone());
+					return response;
+				});
+			});
+		})
+	);
+});
